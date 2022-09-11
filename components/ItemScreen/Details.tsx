3b63410a4426cd5b216780666fa39a4cef2c310f
William Grigor <bjgrigor@gmail.com>
2022-09-11 01:13:18 -0500

    Split buttons now conditionally render depending on progress

--- components/ItemScreen/Details.tsx
+++ components/ItemScreen/Details.tsx
@@ -2,22 +2,18 @@ import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
 import ButtonGroup from "@mui/material/ButtonGroup"
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import { useContext, useEffect, type FC } from 'react'
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
+import { useContext, useEffect, type FC } from "react"
 
-import { ItemScreenContext } from '.'
-import { StepTrackerContext } from '../../pages/cook'
+import { ItemScreenContext } from "."
+import { StepTrackerContext } from "../../pages/cook"
 
 const Details: FC = () => {
     const tracker = useContext(StepTrackerContext)
     const item = useContext(ItemScreenContext)
     const activeStep = tracker[item.Order.id][item.Item.id]
     const task = item.Item.Task[activeStep]
-
-    useEffect(() => {
-        console.log('New active step:', activeStep)
-    }, [activeStep])
 
     const handleNext = () => {
         tracker.updateStep(item.Order.id, item.Item.id, 1)
@@ -28,12 +24,14 @@ const Details: FC = () => {
     }
 
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            minhHeight: '100%'
-        }}>
-            { task ? (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                minhHeight: "100%",
+            }}
+        >
+            {task ? (
                 <>
                     <Typography variant="h4">{task.name}</Typography>
                     <Typography variant="body1">{task.body}</Typography>
@@ -44,11 +42,28 @@ const Details: FC = () => {
                 </>
             )}
 
-            <ButtonGroup sx={{ marginTop: 'auto'}} fullWidth>
-                <Button size="large" startIcon={<ArrowBackIosIcon />} variant="contained" onClick={handleBack}>Last Step</Button>
-                <Button size="large" endIcon={<ArrowForwardIosIcon />} variant="contained" onClick={handleNext}>Next Step</Button>
+            <ButtonGroup sx={{ marginTop: "auto" }} fullWidth>
+                {activeStep > 0 && (
+                    <Button
+                        size="large"
+                        startIcon={<ArrowBackIosIcon />}
+                        variant="contained"
+                        onClick={handleBack}
+                    >
+                        Last Step
+                    </Button>
+                )}
+                {activeStep < item.Item.Task.length && (
+                    <Button
+                        size="large"
+                        endIcon={<ArrowForwardIosIcon />}
+                        variant="contained"
+                        onClick={handleNext}
+                    >
+                        Next Step
+                    </Button>
+                )}
             </ButtonGroup>
-
         </Box>
     )
 }
