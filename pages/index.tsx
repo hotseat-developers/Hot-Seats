5ca893351aaa7a4f469241c1e0769bff98e17410
Brandon! <brandonmw1@hotmail.com>
2022-09-14 21:58:52 -0700

    added re-route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from "next"
 import { Typography } from "@mui/material"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react";
+import Router from 'next/router'
+
+
+
 
 const Home: NextPage = () => {
+    useEffect(() => {
+   const {pathname} = Router
+   if(pathname == '/' ){
+       Router.push('/selection')
+   }
+ });
     return (
         <>
             <Typography variant="body1">Welcome to HotSeat!</Typography>
+
         </>
     )
 }
